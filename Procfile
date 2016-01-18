7063857369936c0a9d077cf3ccff2c2262eecd3b
Kiet Nguyen <kietnguyen86@gmail.com>
2016-01-17 17:02:33 -0800

    Fix missing bson module in mongodb

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./node_modules/.bin/forever app.js
+web: (cd ./node_modules/mongodb && npm install) && ./node_modules/.bin/forever app.js
--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser(process.env.COOKIE_SECRET || 'your secret here'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
